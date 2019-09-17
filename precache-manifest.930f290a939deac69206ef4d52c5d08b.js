60a9817fd694d0c208ab89bbad3cfc456b51556a
Eugene Volkov <eugenewolf507@gmail.com>
2019-09-17 16:58:32 +0300

    Updates

---
+++ precache-manifest.930f290a939deac69206ef4d52c5d08b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5141f24840c236ca512bc0908f8a8b48",
+    "url": "/goit-react-hw-04-reader/index.html"
+  },
+  {
+    "revision": "aed2f9621d2b079c8026",
+    "url": "/goit-react-hw-04-reader/static/css/main.d87b3bce.chunk.css"
+  },
+  {
+    "revision": "91515c027a5e5f0cf75e",
+    "url": "/goit-react-hw-04-reader/static/js/2.dfbcef68.chunk.js"
+  },
+  {
+    "revision": "aed2f9621d2b079c8026",
+    "url": "/goit-react-hw-04-reader/static/js/main.5d4f622f.chunk.js"
+  },
+  {
+    "revision": "84bf89e08b01550b9b7d",
+    "url": "/goit-react-hw-04-reader/static/js/runtime~main.860f1cd1.js"
+  }
+]);
